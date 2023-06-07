e4b8d1145a97299dfeb8ad7bba855f69b30dbefa
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-06-07 22:31:44 +0600

    db connect

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config()
 
@@ -8,6 +9,30 @@ const app = express();
 // middleware 
 app.use(cors());
 app.use(express.json());
+
+// Connect the client to the server
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@4meprogramming.mp2bykf.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 // server listening 
 app.listen(PORT, ()=> {
